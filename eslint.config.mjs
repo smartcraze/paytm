936ac7d8213c581269dacc739b060324f4aa7614
smartcraze <smartcraze354@gmail.com>
2025-02-12 01:06:42 +0530

    unused vars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
 
       "@typescript-eslint/no-explicit-any": "off",
       'react/no-unescaped-entities': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
     },
   },
 ];
